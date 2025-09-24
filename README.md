2ab96334a452adf7d20ae80212a959d98d56658d
ldevivo3 <ldevivo3@gatech.edu>
2025-09-23 21:57:10 -0400

    Backend API started. OAuth setup to Google Calendar API. Succesfully receiving events JSON.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Busyness Buster
 Personal AI-powered productivity agent to review your calendar, tasks and goals to make sure you're working on business, not just busyness.
 Coming soon...
+
+** Back-End **
+We will build a RESTful API integration using FASTAPI to get calendar JSONS which we can fetch for individual events over a period of time (Unsure if it will be next 1 day, previous day, previous week as I usually just schedule my days the night before then throughout the day ). Identify the endpoints and build proceessing ipeline to ensure smooth scraping of events to store in the database. Next steps will be to implement openAI API calls for AI analysis of the info. 
--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, CheckConstraint
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, CheckConstraint, Enum
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
@@ -18,24 +18,41 @@ class Task(Base):
         CheckConstraint('priority >= 0 AND priority <= 10', name='priority_range'),
     )
 
+class Goal(Base): 
+    __tablename__ = 'goals'
+    id = Column(Integer, primary_key = True)
+    goal = Column(String, nullable = False)
+    priority = Column(Integer, default = 0)
+    accomplished = Column(Boolean, default = False)
+    forecast = Column(Enum('Short', 'Medium', 'Long', name='forecast_enum'))
+
+    __table_args__ = (
+        CheckConstraint('priority >= 0 AND priority <= 10', name='priority_range'),
+    )
+
 class Event(Base):
     __tablename__ = 'events'
 
     id = Column(Integer, primary_key = True)
-    name = Column(String, nullable = False)
-    time = Column(DateTime)
-
+    google_id = Column(String, nullable = False, unique = True)
+    summary = Column(String, nullable = False)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
 
 engine = create_engine("sqlite:///busyness.db", echo=True)
 Base.metadata.create_all(engine)
 
-test_task = Task(title = "Finish Busyness Buster!", due_date = datetime(2025, 9, 22), priority = 10)
-test_event = Event(name = "Study for GA", time = datetime(2025,9,23,14,30))
-
 Session = sessionmaker(bind=engine)
 session = Session()
 
+""" 
+test_task = Task(title = "Finish Busyness Buster!", due_date = datetime(2025, 9, 22), priority = 10)
+test_event = Event(name = "Study for GA", time = datetime(2025,9,23,14,30))
+test_goal = Goal(goal = 'Get a job', priority = 10, forecast = 'Short')
+
 session.add(test_task)
-session.add(test_task)
+session.add(test_event)
+session.add(test_goal)
+"""
 
 session.commit()
